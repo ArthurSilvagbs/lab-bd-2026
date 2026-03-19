cc50b54b29ee560b1969191f220ced2059f20f1d
ARTHUR GABRIEL BORGES SILVA <arthur.gsilva@a.ucb.br>
2026-03-18 21:07:29 -0300

    salvando

--- atividade1/create-table-livro.sql
+++ atividade1/create-table-livro.sql
@@ -7,3 +7,18 @@ CREATE TABLE Livro (
         categoria VARCHAR(128),
         ano_publicacao YEAR NOT NULL
 );
+
+ALTER TABLE livro ADD COLUMN status_leitura VARCHAR(255);
+ALTER TABLE livro DROP status_leitura;
+ALTER TABLE livro MODIFY status_leitura BOOLEAN DEFAULT(0);
+
+DESCRIBE livro;
+ALTER TABLE livro ADD CONSTRAINT check_status_livro CHECK (status_leitura IN (0, 1) );
+
+ALTER TABLE livro
+ADD COLUMN id_usuario INT, 
+ADD CONSTRAINT fk_livro_usuario 
+FOREIGN KEY (id_usuario) REFERENCES usuario(id_usuario) 
+ON DELETE CASCADE;
+
+
